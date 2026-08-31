d5675a94cf7e69448f0402f112fb5d61c54ce2ed
JONGWOOK2928 <a01029289121@gmail.com>
2026-08-31 12:17:21 +0900

    pageHeader 교체 및 주석처리 구문 삭제,  수정할 첨부파일 칸 나눔
    pageHeader 교체 및 주석처리 구문 삭제,  update 에서 첨부파일 기존과 수정할 첨부파일 칸 나눔

--- src/notice/components/NoticeItemComponent.jsx
+++ src/notice/components/NoticeItemComponent.jsx
@@ -6,19 +6,11 @@ export default function NoticeItemComponent(props) {
     // 실행할 구문
     let navigate = useNavigate();
 
-    // props = {item : {여기}};
     const item = props.item;
 
     // return 구문
     return (
 
-        // <tr onClick={ () =>  { navigate(`/notices/${ item.noticeId }`); } }>
-        // <td>{ item.noticeId }</td>
-        // <td>{ item.title }</td>
-        // <td>{ item.employee.name }</td>
-        // <td>{ item.createdAt?.substring(0, 10) }</td>
-        // <td>{ item.count }</td>
-        // </tr>
         <TableRow key={item.noticeId} onClick={() => navigate(`/notices/${ item.noticeId }`)}>
             <TableCell className="font-medium">{item.noticeId}</TableCell>
             <TableCell>{item.title}</TableCell>
