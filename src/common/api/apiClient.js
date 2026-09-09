0b3f46f4fdf71a3c5d3f145f320ac5202f584910
송경민 <songkm2@naver.com>
2026-09-09 14:26:56 +0900

    refactor: 토큰 전달 방식 변경으로 인해 사용하지 않게 된 JWT 인터셉터 제거

--- src/common/api/apiClient.js
+++ src/common/api/apiClient.js
@@ -7,17 +7,6 @@ export const apiClient = axios.create({
   baseURL: "/healthgate",
 });
 
-// JWT 토큰 주입
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 // RFC 9457 에러 파싱
 apiClient.interceptors.response.use(
   (response) => response,
